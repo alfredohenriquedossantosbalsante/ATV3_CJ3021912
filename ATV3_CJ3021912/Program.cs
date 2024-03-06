048004d408c1bdb0725ebf3823f2121bd5665c9a
CJ3021912 <CJ3021912@LAB01-PC05>
2024-03-06 08:54:45 -0300

    Adicionar arquivos de projeto.

---
+++ ATV3_CJ3021912/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ATV3_CJ3021912
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float nota;
+            Console.WriteLine("insira a sua nota: ");
+            nota = float.Parse(Console.ReadLine());
+            if (nota >= 6.0)
+            {
+                Console.WriteLine("voce passou");
+            }
+            else { Console.WriteLine("voce reprovou"); }
+
+        }
+    }
+}
